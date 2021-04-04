bdb18aa062204f1d8563de3fb8dadd749df9bd32
SiWhitt <80222749+SiWhitt@users.noreply.github.com>
2021-04-04 10:00:49 -0400

    Adding water and places to the map of Ireland

--- GIS_Tool.py
+++ GIS_Tool.py
@@ -1,13 +1,15 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
+
 import pandas as pd
+from cartopy.feature import ShapelyFeature
 from shapely.geometry import Point, LineString, Polygon
 
-#%matplotlib notebook #not sure if this is required for the work that I am doing
+
+# %matplotlib notebook #not sure if this is required for the work that I am doing
 
 plt.ion() # make the plotting interactive
 
@@ -18,6 +20,7 @@ def generate_handles(labels, colors, edge='k', alpha=1):
     for i in range(len(labels)):
         handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
     return handles
+
 
 # create a scale bar of length 20 km in the upper right corner of the map
 def scale_bar(ax, location=(0.92, 0.95)):
@@ -32,14 +35,62 @@ def scale_bar(ax, location=(0.92, 0.95)):
 
     plt.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=tmc)
     plt.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=tmc)
-    plt.plot([sbx-10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=tmc)
+    plt.plot([sbx - 10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=tmc)
 
-    plt.text(sbx, sby-4500, '20 km', transform=tmc, fontsize=8)
-    plt.text(sbx-12500, sby-4500, '10 km', transform=tmc, fontsize=8)
-    plt.text(sbx-24500, sby-4500, '0 km', transform=tmc, fontsize=8)
+    plt.text(sbx, sby - 4500, '20 km', transform=tmc, fontsize=8)
+    plt.text(sbx - 12500, sby - 4500, '10 km', transform=tmc, fontsize=8)
+    plt.text(sbx - 24500, sby - 4500, '0 km', transform=tmc, fontsize=8)
+
 
 # load the outline of Northern Ireland and Ireland
-outline = gpd.read_file('data_files/NI_outline.shp')
+outline = gpd.read_file('data_files/Ireland.shp')
+outline = outline.to_crs(epsg=2158)
+
+#load datasets for display on map
+water = gpd.read_file('data_files/Ire_Water.shp')
+water = water.to_crs(epsg=2158)
+
+counties = gpd.read_file('data_files/Ire_Counties.shp')
+counties = counties.to_crs(epsg=2158)
 
 
 
+
+# create a figure of size 20x60 (representing the page size in inches)
+myFig = plt.figure(figsize=(25, 65))
+
+myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
+ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
+# projection, where we can actually plot our data.
+
+# first, we just add the outline of Northern Ireland using cartopy's ShapelyFeature
+outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
+xmin, ymin, xmax, ymax = outline.total_bounds
+ax.add_feature(outline_feature)  # add the features we've created to the map.
+
+ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS) # set the extent to the boundaries of the NI outline
+
+gridlines = ax.gridlines(draw_labels=True,
+                         xlocs=[-10.5, -10, -9.5, -9, -8.5, -8, -7.5, -7, -6.5, -6, -5.5],
+                         ylocs=[51, 51.5, 52, 52.5, 53, 53.5, 54, 54.5, 55])
+gridlines.left_labels = False # turn off the left-side labels
+gridlines.bottom_labels = False # turn off the bottom labels
+
+#add datafiles to map
+Counties = ShapelyFeature(counties['geometry'], myCRS,
+                            edgecolor='k',
+                            facecolor='w',
+                            linewidth=1)
+ax.add_feature(Counties)
+
+Waterbodies = ShapelyFeature(water['geometry'], myCRS,
+                            edgecolor='mediumblue',
+                            facecolor='mediumblue',
+                            linewidth=1)
+ax.add_feature(Waterbodies)
+
+scale_bar(ax) #Add scale bar to map
+
+ax. set(title='Island of Ireland with Towns and Shipwreck Locations') # Apply Title to Map of Ireland with Towns
+
+myFig.savefig('map.png', bbox_inches='tight', dpi=300) #Save Map of Ireland as png file
