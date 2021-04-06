c0a6f009ee651a93ac96674cc77a2a305a0ff5a8
SiWhitt <80222749+SiWhitt@users.noreply.github.com>
2021-04-06 14:54:47 -0400

    Scale bar modification

--- GIS_Tool.py
+++ GIS_Tool.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
-import arcpy
+
 
 import pandas as pd
 from cartopy.feature import ShapelyFeature
@@ -23,8 +23,8 @@ def generate_handles(labels, colors, edge='k', alpha=1):
     return handles
 
 
-# create a scale bar of length 20 km in the upper right corner of the map
-def scale_bar(ax, location=(0.92, 0.95)):
+# create a scale bar of length 40 km in the lower right corner of the map
+def scale_bar(ax, location=(0.9, 0.05)):
     llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
     sbllx = (llx1 + llx0) / 2
     sblly = lly0 + (lly1 - lly0) * location[1]
@@ -34,13 +34,13 @@ def scale_bar(ax, location=(0.92, 0.95)):
     sbx = x0 + (x1 - x0) * location[0]
     sby = y0 + (y1 - y0) * location[1]
 
-    plt.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=tmc)
-    plt.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=tmc)
-    plt.plot([sbx - 10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=tmc)
+    plt.plot([sbx, sbx - 40000], [sby, sby], color='k', linewidth=9, transform=tmc)
+    plt.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=6, transform=tmc)
+    plt.plot([sbx - 20000, sbx - 40000], [sby, sby], color='w', linewidth=6, transform=tmc)
 
-    plt.text(sbx, sby - 4500, '20 km', transform=tmc, fontsize=8)
-    plt.text(sbx - 12500, sby - 4500, '10 km', transform=tmc, fontsize=8)
-    plt.text(sbx - 24500, sby - 4500, '0 km', transform=tmc, fontsize=8)
+    plt.text(sbx, sby - 8000, '40 km', transform=tmc, fontsize=6)
+    plt.text(sbx - 25000, sby - 8000, '20 km', transform=tmc, fontsize=6)
+    plt.text(sbx - 50000, sby - 8000, '0 km', transform=tmc, fontsize=6)
 
 
 # load the outline of Northern Ireland and Ireland
@@ -48,8 +48,8 @@ outline = gpd.read_file('data_files/Ireland.shp')
 outline = outline.to_crs(epsg=2158)
 
 #load datasets for display on map
-water = gpd.read_file('data_files/Ire_Water.shp')
-water = water.to_crs(epsg=2158)
+#water = gpd.read_file('data_files/Ire_Water.shp')
+#water = water.to_crs(epsg=2158)
 
 counties = gpd.read_file('data_files/Ire_Counties.shp')
 counties = counties.to_crs(epsg=2158)
@@ -58,7 +58,7 @@ counties = counties.to_crs(epsg=2158)
 
 
 # create a figure of size 20x60 (representing the page size in inches)
-myFig = plt.figure(figsize=(25, 65))
+myFig = plt.figure(figsize=(10, 30))
 
 myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
 ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
@@ -83,12 +83,15 @@ Counties = ShapelyFeature(counties['geometry'], myCRS,
                             facecolor='w',
                             linewidth=1)
 ax.add_feature(Counties)
-
+""" Adding Water bodies to the map
 Waterbodies = ShapelyFeature(water['geometry'], myCRS,
                             edgecolor='mediumblue',
                             facecolor='mediumblue',
                             linewidth=1)
 ax.add_feature(Waterbodies)
+
+"""
+
 
 scale_bar(ax) #Add scale bar to map
 
