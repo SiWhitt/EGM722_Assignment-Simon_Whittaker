315ed51ceec05e87d5e99af9cf658f7bb311a4c8
SiWhitt <80222749+SiWhitt@users.noreply.github.com>
2021-04-21 20:29:05 -0400

    creating choropleth map
    map  showing area of inland waterbodies per county

--- GIS_Tool.py
+++ GIS_Tool.py
@@ -1,22 +1,23 @@
+import matplotlib.lines as mlines
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import matplotlib.lines as mlines
-import pandas as pd
-from shapely.geometry import Point, LineString, Polygon
-import pandas as pd
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
+import pandas as pd
 
 
 # generate matplotlib handles to create a legend of the features we put in our map.
+
 def generate_handles(labels, colors, edge='k', alpha=1):
     lc = len(colors)  # get the length of the color list
     handles = []
+    i: int
     for i in range(len(labels)):
         handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
     return handles
+
 
 # create a scale bar of length 40 km in the lower right corner of the map
 def scale_bar(ax, location=(0.9, 0.05)):
@@ -38,14 +39,11 @@ def scale_bar(ax, location=(0.9, 0.05)):
     plt.text(sbx - 50000, sby - 8000, '0 km', transform=tmc, fontsize=5)
 
 
-
 # load the outline of Northern Ireland and Ireland
 outline = gpd.read_file('data_files/Simplified_Shapes/Ireland.shp')
 outline = outline.to_crs(epsg=2158)
 
-
-
-#load datasets for display on map
+# load datasets for display on map
 water = gpd.read_file('data_files/Files_for_analysis/Ire_Water.shp')
 water = water.to_crs(epsg=2158)
 
@@ -61,6 +59,9 @@ center_counties = center_counties.to_crs(epsg=2158)
 towns = gpd.read_file('data_files/Files_for_analysis/Ire_Places.shp')
 towns = towns.to_crs(epsg=2158)
 
+water_per_county = gpd.read_file('data_files/Files_for_analysis/water_per_county.shp')
+# water_per_county = gpd.read_file('data_files/Files_for_analysis/water_per_county.shp')
+# water_per_county = water_per_county.to_crs(epsg=2158)
 
 # create a figure of size 20x60 (representing the page size in inches)
 myFig = plt.figure(figsize=(10, 30))
@@ -74,35 +75,33 @@ outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', face
 xmin, ymin, xmax, ymax = outline.total_bounds
 ax.add_feature(outline_feature)  # add the features we've created to the map.
 
-ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS) # set the extent to the boundaries of the NI outline
+ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS)  # set the extent to the boundaries of the NI outline
 
 gridlines = ax.gridlines(draw_labels=True,
                          xlocs=[-10.5, -10, -9.5, -9, -8.5, -8, -7.5, -7, -6.5, -6, -5.5],
                          ylocs=[51, 51.5, 52, 52.5, 53, 53.5, 54, 54.5, 55])
-gridlines.right_labels = False # turn off the left-side labels
-gridlines.top_labels = False # turn off the bottom labels
+gridlines.right_labels = False  # turn off the left-side labels
+gridlines.top_labels = False  # turn off the bottom labels
 
-#add datafiles to map
+# add datafiles to map
 
 """ Adding Ireland Counties to Map """
 Counties = ShapelyFeature(counties['geometry'], myCRS,
-                            edgecolor='k',
-                            facecolor='w',
-                            linewidth=0.25)
+                          edgecolor='k',
+                          facecolor='w',
+                          linewidth=0.25)
 ax.add_feature(Counties)
 
-
 # get a list of unique names for the county boundaries
-#county_names = list(counties.name.unique())
-#county_names.sort()  # sort the counties alphabetically by name
-
+# county_names = list(counties.name.unique())
+# county_names.sort()  # sort the counties alphabetically by name
 
 
 """Adding Water bodies to the map"""
 Waterbodies = ShapelyFeature(water['geometry'], myCRS,
-                            edgecolor='mediumblue',
-                            facecolor='mediumblue',
-                            linewidth=1)
+                             edgecolor='mediumblue',
+                             facecolor='mediumblue',
+                             linewidth=1)
 ax.add_feature(Waterbodies)
 
 Rivers = ShapelyFeature(rivers['geometry'], myCRS,
@@ -115,16 +114,16 @@ ax.add_feature(Rivers)
 # ShapelyFeature creates a polygon, so for point data we can just use ax.plot()
 # town_handle = ax.plot(towns.geometry.x, towns.geometry.y, 's', color='0.5', ms=6, transform=myCRS)
 
-#town_handle = ax.plot(towns[towns['fclass'] == 'town'].geometry.x, towns[towns['fclass'] == 'town'].geometry.y, 's', color='0', ms=6, transform=myCRS)
+# town_handle = ax.plot(towns[towns['fclass'] == 'town'].geometry.x, towns[towns['fclass'] == 'town'].geometry.y, 's', color='0', ms=6, transform=myCRS)
 
-city_handle = ax.plot(towns[towns['fclass'] == 'city'].geometry.x, towns[towns['fclass'] == 'city'].geometry.y, '*', color='r', ms=6, transform=myCRS)
+city_handle = ax.plot(towns[towns['fclass'] == 'city'].geometry.x, towns[towns['fclass'] == 'city'].geometry.y, '*',
+                      color='r', ms=6, transform=myCRS)
 
 # note: if you change the color you use to display lakes, you'll want to change it here, too
 water_handle = generate_handles(['Waterbodies'], ['mediumblue'])
 
 # note: if you change the color you use to display rivers, you'll want to change it here, too
 rivers_handle = [mlines.Line2D([], [], color='royalblue')]  # have to make this a list
-
 
 """
 # add the text labels for the towns
@@ -141,15 +140,14 @@ for i, row in towns.iterrows():
 """
 for i, row in center_counties.iterrows():
     x, y = row.geometry.x, row.geometry.y
-    plt.text(x, y, row['name'].title(), fontsize=4, transform=myCRS) # use plt.text to place a label at x,y
-
+    plt.text(x, y, row['name'].title(), fontsize=4, transform=myCRS)  # use plt.text to place a label at x,y
 
 # ax.legend() takes a list of handles and a list of labels corresponding to the objects you want to add to the legend
 handles = city_handle + water_handle + rivers_handle
 labels = ['Cities', 'Waterbodies', 'Rivers']
 
 leg = ax.legend(handles, labels, title='Legend', title_fontsize=14,
-                 fontsize=12, loc='upper left', frameon=True, framealpha=1)
+                fontsize=12, loc='upper left', frameon=True, framealpha=1)
 
 """
 handles = county_handles + water_handle + river_handle + town_handle + city_handle
@@ -161,11 +159,11 @@ leg = ax.legend(handles, labels, title='Legend', title_fontsize=14,
 
 """
 
-scale_bar(ax) #Add scale bar to map
+scale_bar(ax)  # Add scale bar to map
 
-ax.set(title='Points of Interest within 20km of holiday day trip loction') # Apply Title to Map of Ireland with Towns
+ax.set(title='Points of Interest within 20km of holiday day trip loction')  # Apply Title to Map of Ireland with Towns
 
-myFig.savefig('map.png', bbox_inches='tight', dpi=300) #Save Map of Ireland as png file
+myFig.savefig('Outputs/Planning Map.png', bbox_inches='tight', dpi=300)  # Save Map of Ireland as png file
 
 """The following lines of code use the rivers shapefile alongwith the counties shaprfile to produce a barchart
 depicting the total lenght of rivers in each county in ireland and Northern Ireland"""
@@ -173,28 +171,28 @@ depicting the total lenght of rivers in each county in ireland and Northern Irel
 for i, row in rivers.iterrows():  # iterate over each row in the GeoDataFrame
     rivers.loc[i, 'Length'] = row['geometry'].length  # assign the row's geometry length to a new column, Length
 
-#print(rivers.head())  # print the updated GeoDataFrame to see the changes
+# print(rivers.head())  # print the updated GeoDataFrame to see the changes
 
-join = gpd.sjoin(rivers, counties) #, how='inner', lsuffix='left', rsuffix='right') # perform the spatial join
+join = gpd.sjoin(rivers, counties)  # , how='inner', lsuffix='left', rsuffix='right') # perform the spatial join
 
-join_total = join['Length'].sum() # find the total length of roads in the join GeoDataFrame
+join_total = join['Length'].sum()  # find the total length of roads in the join GeoDataFrame
 
-clipped = [] # initialize an empty list
+clipped = []  # initialize an empty list
 for county in counties['name'].unique():
-    tmp_clip = gpd.clip(rivers, counties[counties['name'] == county]) # clip the roads by county border
+    tmp_clip = gpd.clip(rivers, counties[counties['name'] == county])  # clip the roads by county border
     for i, row in tmp_clip.iterrows():
-        tmp_clip.loc[i, 'Length'] = row['geometry'].length # we have to update the length for any clipped roads
-        tmp_clip.loc[i, 'name'] = county # set the county name for each road feature
-    clipped.append(tmp_clip) # add the clipped GeoDataFrame to the
+        tmp_clip.loc[i, 'Length'] = row['geometry'].length  # we have to update the length for any clipped roads
+        tmp_clip.loc[i, 'name'] = county  # set the county name for each road feature
+    clipped.append(tmp_clip)  # add the clipped GeoDataFrame to the
 
 # pandas has a function, concat, which will combine (concatenate) a list of DataFrames (or GeoDataFrames)
 # we can then create a GeoDataFrame from the combined DataFrame, as the combined DataFrame will have a geometry column.
 clipped_gdf = gpd.GeoDataFrame(pd.concat(clipped))
 clip_total = clipped_gdf['Length'].sum()
 
-#print (clipped_gdf.groupby('name') [['Length']].sum()/1000)
+# print (clipped_gdf.groupby('name') [['Length']].sum()/1000)
 
-total_rivers_per_county = (clipped_gdf.groupby('name') [['Length']].sum()/1000)
+total_rivers_per_county = (clipped_gdf.groupby('name')[['Length']].sum() / 1000)
 
 fig = total_rivers_per_county.plot(kind='bar', width=0.8, rot=90, title="Total Length of Rivers in Irish Counties")
 
@@ -204,13 +202,25 @@ plt.grid(which='both', axis='y', linestyle='-', linewidth='0.5', color='black')
 plt.xlabel('Counties')
 plt.ylabel('Total Length of Rivers')
 
+plt.subplots_adjust(bottom=0.25, left=0.2)
 
-plt.subplots_adjust(bottom = 0.25, left = 0.2)
-
-#plt.show
+# plt.show
 
 chart = fig.get_figure()
-chart.savefig("Total Length of rivers in Irish Counties.png", transparent=True, dpi=300)
+chart.savefig('Outputs/Total Length of rivers in Irish Counties.png', transparent=True, dpi=300)
 
+"""The following lines of code create a chloropleth map of the inland water area of counties in ireland 
+to determine which would be best to visit for a fishing trip"""
 
+fig, ax = plt.subplots(1, figsize=(12, 18))
+# to make a nice colorbar that stays in line with our map, use these lines:
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
+ax = water_per_county.plot(column='sum_area_s', ax=ax, vmin=0, vmax=275, cmap='bone_r', edgecolor='k',
+                           legend=True, cax=cax, legend_kwds={'label': 'Total Area of Waterbodies in sq km'})
+
+# Switch off the bounding box drawn round the map so it looks a bit tidier
+ax.axis('off')
+
+fig.savefig('Outputs/Total area of inland water per county in ireland.png', dpi=300, bbox_inches='tight')
