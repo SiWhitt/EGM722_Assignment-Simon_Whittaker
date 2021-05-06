db2b893329523471b30cdaf8e6189c9dfb9459f1
SiWhitt <80222749+SiWhitt@users.noreply.github.com>
2021-05-06 17:50:20 -0400

    Update GIS_Tool.py
    making code for output 1 more more presentable. Adding descriptions of what each section of code does and re-ordering to make it easier to understand

--- GIS_Tool.py
+++ GIS_Tool.py
@@ -1,3 +1,17 @@
+""" GIS_Tool - This script produces 3 outputs which assist in the planning process for a fly fishing
+trip to Ireland.
+
+First, a planning map in png format will be produced which details Irish counties, significant settlements,
+large waterbodies and contains a scale and co-ordinate system to help determine distance and travel time.
+
+The second part of the script computes the total length of rivers in each county on the Island of Ireland
+and displays this as a bar graph.
+
+Finally a choropleth map will be produced containing the area of inland waterbodies in sq km for each county
+in Ireland.
+"""
+
+# import the required modules to ensue that the scrip runs as intended
 import matplotlib.lines as mlines
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -8,38 +22,7 @@ import matplotlib.patches as mpatches
 import pandas as pd
 
 
-# generate matplotlib handles to create a legend of the features we put in our map.
-
-def generate_handles(labels, colors, edge='k', alpha=1):
-    lc = len(colors)  # get the length of the color list
-    handles = []
-    i: int
-    for i in range(len(labels)):
-        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
-    return handles
-
-
-# create a scale bar of length 40 km in the lower right corner of the map
-def scale_bar(ax, location=(0.9, 0.05)):
-    llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
-    sbllx = (llx1 + llx0) / 2
-    sblly = lly0 + (lly1 - lly0) * location[1]
-
-    tmc = ccrs.Mercator(sbllx, sblly)
-    x0, x1, y0, y1 = ax.get_extent(tmc)
-    sbx = x0 + (x1 - x0) * location[0]
-    sby = y0 + (y1 - y0) * location[1]
-
-    plt.plot([sbx, sbx - 40000], [sby, sby], color='k', linewidth=6, transform=tmc)
-    plt.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=4, transform=tmc)
-    plt.plot([sbx - 20000, sbx - 40000], [sby, sby], color='w', linewidth=4, transform=tmc)
-
-    plt.text(sbx, sby - 8000, '40 km', transform=tmc, fontsize=5)
-    plt.text(sbx - 25000, sby - 8000, '20 km', transform=tmc, fontsize=5)
-    plt.text(sbx - 50000, sby - 8000, '0 km', transform=tmc, fontsize=5)
-
-
-# load the outline of Northern Ireland and Ireland
+# load the required datasets from data_files folder and standardise the CRS of each shapefile.
 outline = gpd.read_file('data_files/Simplified_Shapes/Ireland.shp')
 outline = outline.to_crs(epsg=2158)
 
@@ -60,30 +43,67 @@ towns = towns.to_crs(epsg=2158)
 
 water_per_county = gpd.read_file('data_files/Files_for_analysis/water_per_county.shp')
 
+myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
 
-# create a figure of size 20x60 (representing the page size in inches)
+
+""" Output 1 - The following lines of code produce a map to assist with the trip planning process"""
+
+# generate matplotlib handles for inclusion on the planning map legend highlighting significant features
+
+def generate_handles(labels, colors, edge='k', alpha=1):
+    lc = len(colors)  # get the length of the color list
+    handles = []
+    i: int
+    for i in range(len(labels)):
+        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
+    return handles
+
+
+# create a scale bar of length 40 km to be positioned in the lower right corner of the map
+
+def scale_bar(ax, location=(0.9, 0.05)):
+    llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
+    sbllx = (llx1 + llx0) / 2
+    sblly = lly0 + (lly1 - lly0) * location[1]
+
+    tmc = ccrs.Mercator(sbllx, sblly)
+    x0, x1, y0, y1 = ax.get_extent(tmc)
+    sbx = x0 + (x1 - x0) * location[0]
+    sby = y0 + (y1 - y0) * location[1]
+
+    plt.plot([sbx, sbx - 40000], [sby, sby], color='k', linewidth=6, transform=tmc)
+    plt.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=4, transform=tmc)
+    plt.plot([sbx - 20000, sbx - 40000], [sby, sby], color='w', linewidth=4, transform=tmc)
+
+    plt.text(sbx, sby - 8000, '40 km', transform=tmc, fontsize=5)
+    plt.text(sbx - 25000, sby - 8000, '20 km', transform=tmc, fontsize=5)
+    plt.text(sbx - 50000, sby - 8000, '0 km', transform=tmc, fontsize=5)
+
+
+# create a figure of page size 10 inches x 30 inches
 myFig = plt.figure(figsize=(10, 30))
 
-myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
-ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
-# projection, where we can actually plot our data.
+# create an axes object in output 1 using the mercator projection to allow data to be plotted
+ax = plt.axes(projection=ccrs.Mercator())
 
-# first, we just add the outline of Northern Ireland using cartopy's ShapelyFeature
+
+# define map outline and figure extent. Set outline colors and add outline of Ireland to the map
 outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
 xmin, ymin, xmax, ymax = outline.total_bounds
-ax.add_feature(outline_feature)  # add the features we've created to the map.
+ax.add_feature(outline_feature)
 
-ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS)  # set the extent to the boundaries of the NI outline
+# set map extent to the boundaries the Ireland outline
+ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS)
 
+# add gridlines to the map and modify to show labels on left and bottom sides of map
 gridlines = ax.gridlines(draw_labels=True,
                          xlocs=[-10.5, -10, -9.5, -9, -8.5, -8, -7.5, -7, -6.5, -6, -5.5],
                          ylocs=[51, 51.5, 52, 52.5, 53, 53.5, 54, 54.5, 55])
-gridlines.right_labels = False  # turn off the left-side labels
-gridlines.top_labels = False  # turn off the bottom labels
+gridlines.right_labels = False
+gridlines.top_labels = False
 
-# add datafiles to map
+# adding irish counties to the planing map for formating to look nice
 
-""" Adding Ireland Counties to Map """
 Counties = ShapelyFeature(counties['geometry'], myCRS,
                           edgecolor='k',
                           facecolor='w',
@@ -91,60 +111,51 @@ Counties = ShapelyFeature(counties['geometry'], myCRS,
 ax.add_feature(Counties)
 
 
-
-
-"""Adding Water bodies to the map"""
+# adding significant waterbodies to the planning map and formatting
 Waterbodies = ShapelyFeature(water['geometry'], myCRS,
                              edgecolor='mediumblue',
                              facecolor='mediumblue',
                              linewidth=1)
 ax.add_feature(Waterbodies)
 
-
-# ShapelyFeature creates a polygon, so for point data we can just use ax.plot()
-
-
+# adding significant cities / towns and formatting shape and color of symbols
 town_handle = ax.plot(towns[towns['fclass'] == 'town'].geometry.x, towns[towns['fclass'] == 'town'].geometry.y, 's',
                       color='y', ms=6, transform=myCRS)
 
 city_handle = ax.plot(towns[towns['fclass'] == 'city'].geometry.x, towns[towns['fclass'] == 'city'].geometry.y, '*',
                       color='r', ms=6, transform=myCRS)
 
-
-# note: if you change the color you use to display lakes, you'll want to change it here, too
+# adding waterbodies and setting colour of these features to medium blue
 water_handle = generate_handles(['Waterbodies'], ['mediumblue'])
 
-
-# add the text labels for the towns
+# add the text labels for the significant cities / towns
 for i, row in towns.iterrows():
     x, y = row.geometry.x, row.geometry.y
-    plt.text(x, y, row['name'].title(), fontsize=6, transform=myCRS) # use plt.text to place a label at x,y
+    plt.text(x, y, row['name'].title(), fontsize=6, transform=myCRS)
 
-
-"""
-# add the text labels for the counties
-
-##counties['coords'] = counties['geometry'].apply(lambda x: x.centroid.coords[:])
-##counties['coords'] = [coords[0] for coords in counties['coords']]
-"""
+# add the names of each of the Irish counties in the center of the county
 for i, row in center_counties.iterrows():
     x, y = row.geometry.x, row.geometry.y
-    plt.text(x, y, row['name'].title(), fontsize=4, transform=myCRS)  # use plt.text to place a label at x,y
+    plt.text(x, y, row['name'].title(), fontsize=4, transform=myCRS)
 
-# ax.legend() takes a list of handles and a list of labels corresponding to the objects you want to add to the legend
-handles = city_handle + town_handle + water_handle #+ rivers_handle
-labels = ['Cities', 'Towns', 'Waterbodies'] # 'Rivers']
+# define the handles corresponding to features that will be included in the planning map legend
+handles = city_handle + town_handle + water_handle
+labels = ['Cities', 'Towns', 'Waterbodies']
 
+# create legend from handles, format for presentation and place in upper left corner
 leg = ax.legend(handles, labels, title='Legend', title_fontsize=14,
                 fontsize=12, loc='upper left', frameon=True, framealpha=1)
 
+#add scale bar to map
+scale_bar(ax)
+
+# create and add title to the planning map
+ax.set(title='Map for planning fishing trips in Ireland')
+
+# save the planning map to the data_files folder within the repository at a suitable resolution/dpi
+myFig.savefig('data_files/Planning Map.png', bbox_inches='tight', dpi=300)
 
 
-scale_bar(ax)  # Add scale bar to map
-
-ax.set(title='Map for planning fishing trips in Ireland')  # Apply Title to Map of Ireland with Towns
-
-myFig.savefig('data_files/Planning Map.png', bbox_inches='tight', dpi=300)  # Save Map of Ireland as png file
 
 """The following lines of code use the rivers shapefile alongwith the counties shaprfile to produce a barchart
 depicting the total lenght of rivers in each county in ireland and Northern Ireland"""
